d537f0f487109f1d998af66028c66d589b40e65e
KichanovaK <ks325kich@gmail.com>
2023-11-22 13:58:16 +0300

    chages

--- КичановаКК/Practice3/Practice3/main.c
+++ КичановаКК/Practice3/Practice3/main.c
@@ -4,13 +4,13 @@
 #define N 6
 int main()
 {
-    int n, i, j, num[N], guess[N], cows = 0, bulls = 0;
+    int n, i, j, num1[N],num2[N], guess, cows = 0, bulls = 0;
     do {
         printf("Input number length from 2 to 5: ");
         scanf("%d", &n);
     } while ((n < 2) || (n > 5));
     srand((unsigned int)time(0));
-    num[0] = 1 + rand() % 9;
+    num1[0] = 1 + rand() % 9;
     for (i = 1; i < n; i++)
     {
         while (1)
@@ -18,11 +18,11 @@ int main()
             int digit = rand() % 10;
             int one = 1;
             for (j = 0; j < n; j++)
-                if (num[j] == digit) one = 0;
+                if (num1[j] == digit) one = 0;
 
             if (one)
             {
-                num[i] = digit;
+                num1[i] = digit;
                 break;
             }
         }
@@ -30,14 +30,14 @@ int main()
     do {
         cows = 0; bulls = 0;
         printf("Input your guess:\n");
-        for (i = 0;  i < n; i++) 
-        {
-            printf("%d -", i+1);
-            scanf("%d", &guess[i]);
+        scanf("%d", &guess);
+        for (i = n - 1; i >= 0; i--){
+            num2[i] = guess % 10;
+            guess = guess / 10;
         }
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
-                if (num[i] == guess[j]){
+                if (num2[i] == num1[j]){
                     if (i == j) bulls++;
                     else if (i != j) cows++;
                 }
@@ -48,7 +48,7 @@ int main()
     printf("You win! Number was:");
     for (i = 0; i < n; i++)
     {
-        printf("%d", num[i]);
+        printf("%d", num1[i]);
     }
     return 0;
 }
